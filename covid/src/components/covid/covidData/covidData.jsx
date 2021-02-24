86f9d4fc47494c7bf0b8c6575f035a711046413c
Song jae hyeok <wakij6587@naver.com>
2021-02-24 12:30:21 +0900

    코로나 필요 데이터정리, 업데이트시간 간소화
    코로나 필요 데이터정리, 업데이트시간 간소화

--- covid/src/components/covid/covidData/covidData.jsx
+++ covid/src/components/covid/covidData/covidData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './covidData.module.css';
 
 const CovidData = ({item, dailyDecide}) => {
@@ -7,12 +7,8 @@ const CovidData = ({item, dailyDecide}) => {
         <li className={styles.itemList}>
             {dailyDecide && 
             <ul>
-            <li>날짜 : {item.createDt}</li>
+            <li>날짜 : {item.createDt.split('').splice(0,10).join('')}</li>
             <li>신규 확진자 : {dailyDecide}</li>
-            {/* <li>누적 확진자 : {item.decideCnt}</li>
-            <li>누적 사망자 : {item.deathCnt}</li>
-            <li>누적 격리해제 : {item.clearCnt}</li>
-            <li>누적 환진률 :{item.accDefRate} </li> */}
             <li></li>
             </ul>}
         </li>
